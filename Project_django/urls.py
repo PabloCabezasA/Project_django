0e38aa6e4df38d1f434790f5252e40dd99a3c92f
PabloCabezasA <pabloo.cabezaas@gmail.com>
2016-04-11 13:32:27 -0300

    cambio en urls

--- Project_django/urls.py
+++ Project_django/urls.py
@@ -5,11 +5,9 @@ from django.conf.urls import patterns
 
 
 urlpatterns = [
-    # Examples:
 #para ejemplos     url(r'^$', 'Project_django.views.index', name='index'),
      url(r'^$', 'Project_django.views.user_login', name='login'),
      url(r'^logout/$', 'Project_django.views.user_logout', name='logout'),     
-     url(r'^curso1/', include('Project_django.apps.curso1.urls' , namespace="curso1")),
      url(r'^terminal/', include('Project_django.apps.terminal_venta.urls' , namespace="terminal")),
      url(r'^media/(?P<path>.*)$','django.views.static.serve',
 	       {'document_root':settings.MEDIA_ROOT,}
